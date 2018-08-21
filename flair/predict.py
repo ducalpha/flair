ded036d6f3a7c11b41a7dff00156a7d66cfd66f3
Duc Bui <ducbui@umich.edu>
2018-08-20 23:08:33 -0400

    fix bugs

--- flair/predict.py
+++ flair/predict.py
@@ -13,33 +13,41 @@ from typing import List
 def load_data(sents_file_path):
     """Input: a file with 1 sentence per line.
     Output: a list of sentences."""
-    sents_file_path = Path(sents_file_path)
-    lines = sents_file_path.read_text().split('\n')
-    return [Sentence(line.strip()) for line in lines]
+    #sents_file_path = Path(sents_file_path)
+    #lines = sents_file_path.read_text().split('\n')
+    sents = read_sents_file(sents_file_path)
+    return [Sentence(sent.strip(), use_tokenizer=True) for sent in sents if len(sent) > 0]
 
 def write_tagged_sents(output_file_path, tagged_sents):
     conll03_sents : List[str] = []
     for sent in tagged_sents:
+        assert len(sent) == 1  # tagged_sents should be a list of list of single sentence
+        sent = sent[0]
         conll03_str = []
         for tok in sent.tokens:
             # Default pos and chunk tag are O (other) which are unused.
             conll03_str.append('{} O O {}'.format(tok.text, tok.get_tag(tag_type='ner')))
         conll03_sents.append('\n'.join(conll03_str))
 
-    output_file_path.write_text('\n'.append(conll03_sents))
+    output_file_path.write_text('\n\n'.join(conll03_sents))
 
 def perform_ner(model_file_path, sents_file_path):
     # Load the pre-trained model from disk.
     tagger : SequenceTagger = SequenceTagger.load_from_file(model_file_path)
 
     # Make prediction.
-    sentences = read_sents_file(sents_file_path)
-    tagged_sentences = tagger.predict(sentences)
+    sentences = load_data(sents_file_path)
+    #assert all([len(sent) for sent in sentences])
+    #import pdb; pdb.set_trace()
+    tagged_sents = []
+    for sent in sentences:
+        tagged_sents.append(tagger.predict(sent))
 
     # Write output.
     sents_file_path = Path(sents_file_path)
     output_file_path = sents_file_path.with_suffix('.ner.conll03')
-    write_tagged_sents(output_file_path, tagged_sentences)
+    write_tagged_sents(output_file_path, tagged_sents)
+    print('Output to: {}'.format(output_file_path))
     return output_file_path
 
 def default_perform_ner(sents_file_path):
--- predict.py
+++ predict.py
@@ -1,10 +1,10 @@
 from flair.data import Sentence
 from flair.models import SequenceTagger
 
-# tagger: SequenceTagger = SequenceTagger.load('ner')
-tagger: SequenceTagger = SequenceTagger.load_from_file('resources/taggers/example-ner/final-model.pt')
+tagger: SequenceTagger = SequenceTagger.load('ner')
+# tagger: SequenceTagger = SequenceTagger.load_from_file('resources/taggers/example-ner/final-model.pt')
 
-sentence: Sentence = Sentence('George Washington went to Washington .')
+sentence: Sentence = Sentence('George  Washington went to Washington .')
 tagger.predict(sentence)
 
 print('Analysing %s' % sentence)
